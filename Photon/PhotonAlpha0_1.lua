3a97d26644b77feb0e8ffba177580ab86b886e35
TheAio <76646135+TheAio@users.noreply.github.com>
2024-09-21 15:18:42 +0200

    Rewrite of the entire script
    Made it easier to implement extentions and introduced keybindings.
    Just start typing to write any command,
    press the arrow up to run the last command again or
    press C to run shortcuts, plugins and hardcoded extra features

--- Photon/PhotonAlpha0_1.lua
+++ Photon/PhotonAlpha0_1.lua
@@ -1,48 +1,51 @@
--- this is alpha release 0.1
--- Photon terminal, the latest and greatest terminal of CC: Tweaked
+--configuration
 local my_window = window.create(term.current(), 1, 1, 20, 9)
 shell.run("clear")
 my_window.clear()
-
 local xpos = term.getCursorPos(x)
 local ypos = term.getCursorPos(y)
-local file = fs.open("/.conf/photon/.conf.photon", "r")
-local lines = {}
-while true do
-  -- terminal
-  local dir = shell.dir()
-  my_window.setTextColour(colors.orange)
-  write(dir .. "> ")
-  my_window.setTextColour(colors.white)
-  local readtxt = read()
-  shell.run(readtxt)
-  local BBar = string.char(16)
-  sleep(0.01)
-  
+letters = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+"1","2","3","4","5","6","7","8","9","0","+","-","_","/",".",",","?","!","@"
+}
+last = ""
 
-  -- filelist
-  while readtxt == "fl" do
-    shell.run("clear")
-    local readtxt = "clear"
-    print("Search:")
-    local files = fs.list("/*/" .. read())
-    for i = 1, #files do
-      print(files[i])
+--functions
+function commandBox()
+    my_window.setTextColour(colors.red)
+    write("> CMD >")
+    my_window.setTextColour(colors.white)
+    readtxt = read()
+    if fs.exists("/conf/photon/conf.photon") then
+        --todo: load conf.photon and any plugins etc
+        printError("SUPPORT FOR CONF.PHOTON NOT IMPLEMENTED")
+        printError("FIX ME! INSIDE FUNC COMMANDBOX")
+    else
+        --here you can hard code plugins, i would recommend not doing it tho.
+        if readtxt == "example" then
+            return "fg ls"
+        end
     end
-    local event, character = os.pullEvent("char")
-    if character == "!" then shell.run("clear") print("File:") shell.run("edit") read() break end
-    if character == "*" then shell.run("clear") break end
-  end
-  --while readtxt == "?" do
-    --shell.run("clear")
-    --print("Index: ")
-    --shell.run(".appdata/photon/help+")
-  --break end
-
-  -- plugins
-  --local file = fs.open("/.conf/photon/Plugin.photon", "r")
-  --local contents = file.readAll()
-  --file.close()
-  --shell.run("clear")
 end
--- todo: add error handling, auto reboot, terminal tiling, plugins, config, better ui, compatibility mode (runs selected terminal for usage), keybinder and help menu
+
+--main loop
+while true do
+    local dir = shell.dir()
+    readtxt = ""
+    my_window.setTextColour(colors.orange)
+    write(shell.dir())
+    my_window.setTextColour(colors.white)
+    e,k = os.pullEvent("key")
+    if k == keys.up then
+        readtxt = last
+    elseif k == keys.c then
+        readtxt = commandBox()
+    else
+        my_window.setTextColour(colors.orange)
+        write("> ")
+        my_window.setTextColour(colors.white)
+        readtxt = read()
+    end
+    last = readtxt
+    shell.run(readtxt)
+    sleep(0.1)
+end
